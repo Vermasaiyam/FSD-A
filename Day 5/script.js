9262d6bd3573f30cbabecaa4f3cb2df6271ba830
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-15 14:47:46 +0530

    day 5

---
+++ Day 5/script.js
@@ -0,0 +1,15 @@
+// let x = confirm("Are you sure to Logout");
+// alert(x);
+
+// let v = prompt("Enter your name","Rahul");
+// alert(v);
+
+let disp = () => console.log("Welcome to arrow function in JS");
+disp();
+let say = () => console.log("I am not saying anything");
+say();
+let sum = (x,y) => {
+    return x + y;
+}
+let res = sum(12,32);
+console.log(res);
