2417196814a699446214745b0c3cf3c8a7a0036f
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-18 10:30:39 +0530

    day 6

--- Day 6/async1.js
+++ Day 6/async1.js
@@ -1,11 +1,35 @@
-console.log("start");
-setTimeout(() => {
-    console.log("first task");
-    setTimeout(() => {
-        console.log("2nd task");
+// console.log("start");
+// setTimeout(() => {
+//     console.log("first task");
+//     setTimeout(() => {
+//         console.log("2nd task");
+//         setTimeout(() => {
+//             console.log("3rd task");
+//         }, 3000);
+//     }, 2000);
+// }, 1000);
+// console.log("END");
+
+
+const myPromise = new Promise((resolve, reject)=>{
+    let success = true;
+    if (success) {
+        resolve("data send success");
+    }
+    else{
+        reject("data failed to success");
+    }
+});
+
+// myPromise.then((msg)=> console.log(msg)).catch((error)=>console.log(error));
+
+function task(msg, delay){
+    return new Promise((resolve)=>{
         setTimeout(() => {
-            console.log("3rd task");
-        }, 3000);
-    }, 2000);
-}, 1000);
-console.log("END");
+            console.log(msg);
+            resolve()
+        }, delay);
+    })
+}
+
+task("1st", 1000).then(()=>task("2nd", 2000)).catch(()=> task("3rd", 3000));
